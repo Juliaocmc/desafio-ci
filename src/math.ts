c53362e3bfc47d2eb81707cad0e1dc1d106711c6
julio.correa <correa_jcm@hotmail.com>
2025-01-26 17:12:46 -0300

    feat: add power and squareRoot

--- src/math.ts
+++ src/math.ts
@@ -15,5 +15,13 @@ export class Math {
     divide(a: number, b: number) {
         return a / b
     }
+
+    power(a: number, b: number) {
+        return a ** b
+    }
+
+    squareRoot(a: number) {
+        return a ** 0.5
+    }
 }
 
