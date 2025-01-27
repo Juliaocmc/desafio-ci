1fd6e3dcecc1f2dcdb6f646f12370a7e75828be9
julio.correa <correa_jcm@hotmail.com>
2025-01-27 07:32:08 -0300

    ci: add --coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["lcov", "text"],
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
